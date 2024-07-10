40885fb82a08ab6cc29ad0ce09b41a0a8c3822b5
Carlos Becerra <carlosalfredo.becerra@mail.polimi.it>
2024-07-10 16:46:32 +0200

    chore: migrated to xml project

---
+++ app/src/main/java/com/loseyourself/firstexercise/HomeFragment.kt
@@ -0,0 +1,2 @@
+package com.loseyourself.firstexercise
+
--- app/src/main/java/com/loseyourself/firstexercise/MainActivity.kt
+++ app/src/main/java/com/loseyourself/firstexercise/MainActivity.kt
@@ -1,46 +1,21 @@
 package com.loseyourself.firstexercise
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.loseyourself.firstexercise.ui.theme.FirstExerciseTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            FirstExerciseTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.textView)
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            textView.text = "Button Clicked!"
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FirstExerciseTheme {
-        Greeting("Android")
-    }
-}
